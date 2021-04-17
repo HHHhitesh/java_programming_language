a766da2408edeb371f9d18b8d3babdacfe2e3075
Hitesh Choudhary <45251268+HHHhitesh@users.noreply.github.com>
2021-04-17 14:04:43 +0530

    Add files via upload

---
+++ math/JavaMathExample.java
@@ -0,0 +1,17 @@
+class JavaMathExample
+{
+	public static void main(String[]args)
+	{
+		double x=28;
+		double y=4;
+		System.out.println("maximum number of x and y is"+Math.max(x,y));
+		System.out.println("square root of :"+Math.sqrt(y));
+		System.out.println("power of x and y :"+Math.pow(x,y));
+		System.out.println("Lograthim of x is : "+Math.log10(x));
+		System.out.println("Lograthim of x is : "+Math.log10(y));
+		System.out.println("exp of a is : "+Math.exp(x));
+
+
+
+	}
+}
